be44e75cc9c0052feb8ddaa89c0f697bdda20327
kennethwty <kennethwty93@gmail.com>
2018-11-17 18:06:23 -0500

    Upload simple thread-safe implementation of the Singleton pattern

--- src/Creational/Singleton.java
+++ src/Creational/Singleton.java
@@ -1,4 +1,47 @@
 package Creational;
 
+/*
+    Author: Tsz Yan "Kenneth" Wong
+
+    Concepts:
+        - Only one instance created
+        - Guarantees control of a resource
+        - Lazily loaded
+        - Examples:
+            - Graphic Manager
+            - Logger
+            - Runtime
+            - Spring Beans
+ */
+
 public class Singleton {
+    private static volatile Singleton me = null;
+
+    private Singleton() {
+        // prevent Reflection
+        if(me != null) {
+            throw new RuntimeException("Use getInstance() to create.");
+        }
+    }
+
+    public static Singleton getInstance() {
+        // lazily loaded
+        if(me == null) {
+            synchronized (Singleton.class) {
+                if(me == null) {
+                    me = new Singleton();
+                }
+            }
+        }
+        return me;
+    }
+
+    public static void main(String[] args) {
+        Singleton lonelyObject = Singleton.getInstance();
+        Singleton robot = Singleton.getInstance();
+
+        if(lonelyObject == robot) {
+            System.out.println("Glad that it is not a lonely person...");
+        }
+    }
 }
