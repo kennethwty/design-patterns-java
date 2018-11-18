72cab2c676fb0de3fc35bae2fda39e3beb33bb9c
kennethwty <kennethwty93@gmail.com>
2018-11-17 19:56:41 -0500

    Upload Builder design pattern with driver

---
+++ src/Creational/Builder.java
@@ -0,0 +1,100 @@
+package Creational;
+
+/*
+    Author: Tsz Yan "Kenneth" Wong
+
+    Concepts:
+        - Great for handling the construction of objects that may contain a lot of parameters
+        - Want to make the objects immutable once they are constructed
+
+        * Large number of parameters
+        * Complex constructors
+        * Immutability
+
+        - Examples:
+            - StringBuilder
+            - DocumentBuilder
+            - Locale.Builder
+ */
+
+public class Builder {
+    public static class OrderBuilder {
+        private int customerId;
+        private String drink = "Coke"; // default
+        private String burger = "Chicken Burger"; // default
+        private boolean fries;
+        private boolean cheese;
+        private String toGo = "Stay"; // default
+
+        public OrderBuilder(int customerId) {
+            this.customerId = customerId;
+        }
+
+        public Builder build() {
+            return new Builder(this);
+        }
+
+        public OrderBuilder setDrink(String drink) {
+            this.drink = drink;
+            return this;
+        }
+
+        public OrderBuilder setBurger(String burger) {
+            this.burger = burger;
+            return this;
+        }
+
+        public OrderBuilder addFries() {
+            this.fries = true;
+            return this;
+        }
+
+        public OrderBuilder addCheese() {
+            this.cheese = true;
+            return this;
+        }
+
+        public OrderBuilder toGoOrStay(String toGoOrNot) {
+            this.toGo = toGoOrNot;
+            return this;
+        }
+    }
+
+    private int customerId;
+    private String drink;
+    private String burger;
+    private boolean fries;
+    private boolean cheese;
+    private String toGo;
+
+    private Builder(OrderBuilder ob) {
+        this.customerId = ob.customerId;
+        this.drink = ob.drink;
+        this.burger = ob.burger;
+        this.cheese = ob.cheese;
+        this.fries = ob.fries;
+        this.toGo = ob.toGo;
+    }
+
+    public int getCustomerId() { return customerId; }
+
+    public String getDrink() {
+        return drink;
+    }
+
+    public String getBurger() {
+        return burger;
+    }
+
+    public boolean getFries() {
+        return fries;
+    }
+
+    public boolean getCheese() {
+        return cheese;
+    }
+
+    public String getToGoOrStay() {
+        return toGo;
+    }
+}
---
+++ src/Creational/BuilderDriver.java
@@ -0,0 +1,21 @@
+package Creational;
+
+/*
+    Driver for the Builder Design Pattern
+ */
+
+public class BuilderDriver {
+    public static void main(String[] args) {
+        Builder.OrderBuilder builder = new Builder.OrderBuilder(1);
+        builder.setDrink("Coffee").setBurger("Double Cheese Burger").addCheese();
+
+        Builder order = builder.build();
+
+        System.out.println("Customer ID: " + order.getCustomerId());
+        System.out.println("Choice of Burger: " + order.getBurger());
+        System.out.println("Choice of Drink: " + order.getDrink());
+        System.out.println("Add Cheese: " + order.getCheese());
+        System.out.println("Add Fries: " + order.getFries());
+        System.out.println("Stay or To-Go: " + order.getToGoOrStay());
+    }
+}
